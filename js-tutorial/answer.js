ef4963fda16e1c6852756f3fdf18bf7abfbe156e
chidinma <chidinmaojimba888@gmail.com>
2025-06-26 09:39:15 -0700

    added answer

---
+++ js-tutorial/answer.js
@@ -0,0 +1,55 @@
+const employee = [{
+            name: "chidinma",
+            age: 10,
+            gender: "female",
+            level: "200",
+            seniority: "Junior",
+            year: "2024",
+            salary: "20000"
+},
+{
+            name: "David",
+            age: 20,
+            gender: "male",
+            level: "100",
+            seniority: "senior",
+            year: "2022",
+            salary: "40000"   
+}
+
+{
+            name: "kossy",
+            age: 50,
+            gender: "male",
+            level: "400",
+            seniority: "senior",
+            year: "2025",
+            salary: "10000"   
+}
+{
+    
+            name: "Maxwell",
+            age: 50,
+            gender: "female",
+            level: "600",
+            seniority: "senior",
+            year: "2021",
+            salary: "50000"   
+}
+
+]
+
+for (value in employee) {
+ // console.log(value, person[value]);
+
+console.log(employee)}
+
+
+
+
+//  - Modify the array of objects (persons) below to contain some other key informations as an employee using the spread operation.
+//     - Informations like: salary, year of employment, seniority e.g junior & senior, qualification
+
+//     - Write a program to log the name, age, gender, and seniority in this format: name age gender seniority
+
+//     - e.g david 29 male senior
